8550894eef21afd54812b784e141bfa55712c580
Thring <179011938@qq.com>
2026-02-06 19:12:39 +0800

    fix
    Removed eval_loader and associated eval method to streamline training process.

--- stage1_trainer.py
+++ stage1_trainer.py
@@ -48,7 +48,6 @@ class Trainer:
                  config,
                  model,
                  train_loader,
-                 eval_loader,
                  log_dir,
                  model_dir,
                  accelerator,
@@ -66,17 +65,13 @@ class Trainer:
 
         self.model = model
         self.train_loader = train_loader
-        self.eval_loader = eval_loader
 
         self.optimizer = get_optimizer(self.model.parameters(), self.config['optimizer'])
-
-
         self.scheduler = get_scheduler(self.optimizer, self.config['optimizer'])
-
+                     
         self.accelerator = accelerator
-
-        self.model, self.optimizer, self.scheduler, self.train_loader, self.eval_loader = self.accelerator.prepare(
-            self.model, self.optimizer, self.scheduler, self.train_loader, self.eval_loader
+        self.model, self.optimizer, self.scheduler, self.train_loader = self.accelerator.prepare(
+            self.model, self.optimizer, self.scheduler, self.train_loader
         )
 
         self.is_main_process = self.accelerator.is_main_process
@@ -88,9 +83,6 @@ class Trainer:
         for epoch in range(self.n_epochs):
             self.train_epoch(epoch)
             self.accelerator.unwrap_model(self.model).reset_codebook_hit()
-            if (epoch + 1) % 5 == 0:
-                self.eval(epoch)
-                self.accelerator.unwrap_model(self.model).reset_codebook_hit()
         if self.is_main_process:
             self.writer.close()
 
@@ -176,70 +168,3 @@ class Trainer:
             rtn['dxy_loss'].item(),
             info
         )
-
-    @torch.no_grad()
-    def eval(self, epoch):
-        self.model.eval()
-
-        loss_list = []
-        recon_loss_list = []
-        quant_loss_list = []
-        percent_loss_list = []
-        dxy_loss_list = []
-
-        info_list = []
-
-        pbar = tqdm(self.eval_loader, ncols=110, disable=not self.accelerator.is_main_process)
-
-        for batch_idx, batch_data in enumerate(pbar):
-            x = batch_data['x'].to(self.accelerator.device)
-            road_seq = batch_data['road_seq'].to(self.accelerator.device)
-            road_gps = batch_data['road_gps'].to(self.accelerator.device)
-            road_gps_len = batch_data['road_gps_len'].to(self.accelerator.device)
-            traj_len = batch_data['traj_len'].to(self.accelerator.device)
-            dxy = batch_data['dxy'].to(self.accelerator.device)
-            road_percent = batch_data['road_percent'].to(self.accelerator.device)
-
-            rtn = self.model(x, traj_len, road_seq, road_gps, road_gps_len, dxy, road_percent)
-            recon_loss, quant_loss, _, info = rtn['recon_loss'], rtn['quant_loss'], rtn['code'], rtn['info']
-            loss = recon_loss + quant_loss
-
-            loss = loss.item()
-            recon_loss = recon_loss.item()
-            quant_loss = quant_loss.item()
-
-            percent_loss = rtn['percent_loss'].item()
-            dxy_loss = rtn['dxy_loss'].item()
-
-            loss_list.append(loss)
-            recon_loss_list.append(recon_loss)
-            quant_loss_list.append(quant_loss)
-            percent_loss_list.append(percent_loss)
-            dxy_loss_list.append(dxy_loss)
-
-            info_list.append(info)
-
-            if self.writer is not None:
-                write_scalar(self.accelerator, self.writer, 'eval/loss', loss, self.eval_global_step)
-                write_scalar(self.accelerator, self.writer, 'eval/recon_loss', recon_loss, self.eval_global_step)
-                write_scalar(self.accelerator, self.writer, 'eval/quant_loss', quant_loss, self.eval_global_step)
-                write_scalar(self.accelerator, self.writer, 'eval/percent_loss', percent_loss, self.eval_global_step)
-                write_scalar(self.accelerator, self.writer, 'eval/dxy_loss', dxy_loss, self.eval_global_step)
-
-            pbar.set_description(f"[Eval  {epoch+1}/{self.n_epochs}]")
-            self.eval_global_step += 1
-
-        avg_loss = np.mean(loss_list)
-        avg_recon_loss = np.mean(recon_loss_list)
-        avg_quant_loss = np.mean(quant_loss_list)
-        avg_percent_loss = np.mean(percent_loss_list)
-        avg_dxy_loss = np.mean(dxy_loss_list)
-
-        avg_info = merge_info(info_list)
-        avg_info_str_list = [', '.join(f"{k}: {v:.8f}" for k, v in info.items()) for info in avg_info]
-
-        log_str(self.accelerator,
-                f"Eval  Ep {epoch+1}/{self.n_epochs} | loss: {avg_loss:.8f}, recon: {avg_recon_loss:.8f}, quant: {avg_quant_loss:.8f}, percent: {avg_percent_loss:.8f}, dxy: {avg_dxy_loss:.8f}")
-
-        for i, avg_info_str in enumerate(avg_info_str_list):
-            log_str(self.accelerator, f"Eval  Ep {epoch+1}/{self.n_epochs}|RQ_Info_{i + 1}: {avg_info_str}")
